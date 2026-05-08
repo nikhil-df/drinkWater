8ee9f2eb0e12e4f11f63a64addbffe9f49b5e3ef
nikhil-df <shaunikhil11@gmail.com>
2026-05-08 10:53:58 +0530

    tested native module (working)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,19 @@
 import { View, Text, Button } from 'react-native'
 import { useColors } from '../hooks/useColors'
+import { NativeModules } from 'react-native';
+
+
 
 export default function HomeScreen() {
+    const { SetAlarm } = NativeModules;
     const colors = useColors()
+    const handleData = () => {
+        SetAlarm.showToast('Hello from Native Modules');
+    }
     return (
         <View style={{ backgroundColor: colors.background , flex: 1}}>
             <Text style={{ color: colors.text }}>HomeScreen</Text>
-            <Button title="Data" onPress={() => {}}/>
+            <Button title="Data" onPress={handleData}/>
         </View>
     )
 }
